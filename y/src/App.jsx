0800fcb99ff2c97bf79e8cfc8835e803fd824d56
samridh <samridhpctebtech19cse@gmail.com>
2023-05-19 10:51:42 +0530

    bootstrap

---
+++ y/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div className="d-flex justify-content-around">
+      <h1 className="px-3">Bootstrap tutorial</h1>
+      <h1 className="text-primary">Hello Vite project</h1>
+    </div>
+  )
+}
+
+export default App
